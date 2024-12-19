6896cf227e0b126bb3bc11073ab0a3d0d25fd89f
edge2992 <torune.summer@gmail.com>
2024-12-19 21:14:16 +0900

    feat: init app typescript

---
+++ test/lambda-java-dynamodb.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaJavaDynamodb from '../lib/lambda-java-dynamodb-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda-java-dynamodb-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaJavaDynamodb.LambdaJavaDynamodbStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
